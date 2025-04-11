d5b4324e4c24c689fc147012f40468238b92ee17
watch <watcharit64@gmail.com>
2025-04-11 19:37:55 +0700

    EP.34

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,7 +1,134 @@
+"use client";
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  InputAdornment,
+  TextField,
+  Typography,
+} from "@mui/material";
+import * as Icons from "@mui/icons-material";
 import React from "react";
+import { Controller, useForm } from "react-hook-form";
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+interface User {
+  username: string;
+  password: string;
+}
 
 type Props = {};
 
 export default function Register({}: Props) {
-  return <div>Register</div>;
+  const initialValue: User = { username: "", password: "" };
+  const formValidateSchema = Yup.object().shape({
+    username: Yup.string().required("Username is required").trim(),
+    password: Yup.string().required("Password is required").trim()
+  });
+
+  const { control, handleSubmit ,formState: {errors}} = useForm<User>({
+    defaultValues: initialValue,
+    resolver: yupResolver(formValidateSchema),
+  });
+
+  const showForm = () => {
+    return (
+      <form onSubmit={handleSubmit((value:User)=> {
+        alert(JSON.stringify(value))
+      })}>
+        {/* Username */}
+        <Controller
+          name="username"
+          control={control}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              variant="outlined"
+              margin="normal"
+              error={(errors.username?.message ?? "") != ""}
+              helperText={errors.username?.message?.toString()}
+              fullWidth
+              slotProps={{
+                input: {
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <Icons.Email />
+                    </InputAdornment>
+                  ),
+                },
+              }}
+              label="Username"
+              autoComplete="email"
+              autoFocus
+            />
+          )}
+        />
+
+        {/* Password */}
+        <Controller
+          name="password"
+          control={control}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              error={(errors.password?.message ?? "") != ""}
+              helperText={errors.password?.message?.toString()}
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              slotProps={{
+                input: {
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <Icons.Password />
+                    </InputAdornment>
+                  ),
+                },
+              }}
+              label="Password"
+              autoComplete="password"
+              autoFocus
+            />
+          )}
+        />
+
+        <Button
+          // sx={{ marginTop: 4 }}
+          className="mt-8"
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+        >
+          Create
+        </Button>
+
+        <Button
+          // sx={{ marginTop: 2 }}
+          className="mt-4"
+          type="button"
+          fullWidth
+          variant="outlined"
+          onClick={() => {}}
+        >
+          Cancel
+        </Button>
+      </form>
+    );
+  };
+
+  return (
+    <Box className="flex justify-center items-center">
+      
+      <Card className="max-w-[345px] mt-[100px]">
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Register
+          </Typography>
+          {showForm()}
+        </CardContent>
+      </Card>
+    </Box>
+  );
 }
